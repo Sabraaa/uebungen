ebad9a334b36cb9ef958d1182ccef18267a94847
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-09 18:58:44 +0330

    useState 2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState("blue");
 
   const decCount = () => {
     setCount((prevCount) => prevCount - 1);
+    setTheme("red");
   };
   const incCount = () => {
     setCount((prevCount) => prevCount + 1);
+    setTheme("green");
     // setCount(count + 1);
   };
   return (
@@ -18,6 +21,9 @@ function App() {
       </button>
       <span style={{ height: "50px", width: "40px", margin: "20px" }}>
         {count}
+      </span>
+      <span style={{ height: "50px", width: "40px", margin: "20px" }}>
+        {theme}
       </span>
       <button style={{ height: "50px", width: "40px" }} onClick={incCount}>
         +

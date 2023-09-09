21a51bf2822172aa01907e397f38d40cc41b63ce
Sabra <100771060+Sabraaa@users.noreply.github.com>
2023-09-09 20:47:36 +0330

    useMemo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,37 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-const handleResize=()=>{
-  setWindowWidth(window.innerWidth)
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = slowFunction(number);
+  const themeStyle = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
+  };
+  return (
+    <div>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button
+        onClick={() => {
+          setDark((prevDark) => !prevDark);
+        }}
+      >
+        click to change theme
+      </button>
+      <div style={themeStyle}>
+          {doubleNumber}
+      </div>
+    </div>
+  );
 }
 
-  useEffect(()=>{
-      window.addEventListener("resize",handleResize)
-      return()=>{
-        window.removeEventListener("resize", handleResize)
-      }
-  },[])
-  return <div>{windowWidth}</div>;
+function slowFunction(num){
+  for (let i = 0; i<1000000; i++ )
+  return num *2
 }
 
 export default App;
